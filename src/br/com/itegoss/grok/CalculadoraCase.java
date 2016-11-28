9528a178aaa05958e4780e4523253c93b42e632c
Rafael Santana <iory20072008@gmail.com>
2016-11-27 22:50:42 -0200

    Criada calculadora com a estrutura de decisão switch, encrementado com a biblioteca JOptionPane.Show

--- src/br/com/itegoss/grok/CalculadoraCase.java
+++ src/br/com/itegoss/grok/CalculadoraCase.java
@@ -1,36 +1,51 @@
 package br.com.itegoss.grok;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class CalculadoraCase {
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		double num1, num2, resu=0;
-		String eq;
-		System.out.println("***************************");
-		System.out.println("* Calculadora de Equações *");
-		System.out.println("***************************");
-		System.out.println("'*' Multiplicação \n'/' Divisão \n'+' Soma \n'-' Subtração");
-		System.out.println("***************************");
-		System.out.println("Informe o tipo da equação");
-		eq = teclado.next();
-		System.out.print("Informe 1º valor: ");
-		num1 = teclado.nextDouble();
-		System.out.print("Informe 2º valor: ");
-		num2 = teclado.nextDouble();
-		if (eq.equals("*")) {
-			resu = num1 * num2;
+		double num1, num2, resu = 0;
+		String simbEq = "";
+		int cont = 1;
+		while (cont == 1) {
+			int eq = Integer.parseInt(JOptionPane.showInputDialog(null,
+					"         ********************************" + "\n             **** CALCULADORA ****"
+							+ "\n                    usando (switch)" + "\n         ********************************"
+							+ "\n1. * Multiplicação    ||    2. / Divisão \n3. + Soma                 ||    4. - Subtração"
+							+ "\n -----------------------------" + "\n Informe o tipo da equação: "));
+			if (eq == 1 || eq == 2 || eq == 3 || eq == 4) {
+				num1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe 1º valor: "));
+				num2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe 2º valor: "));
+				switch (eq) {
+				case 1:
+					resu = num1 * num2;
+					simbEq = "*";
+					break;
+				case 2:
+					resu = num1 / num2;
+					simbEq = "/";
+					break;
+				case 3:
+					resu = num1 + num2;
+					simbEq = "+";
+					break;
+				case 4:
+					resu = num1 - num2;
+					simbEq = "-";
+					break;
+				default:
+					JOptionPane.showMessageDialog(null, "     ERRO!\nVALOR INVALIDO!");
+					break;
+				}
+				JOptionPane.showMessageDialog(null, "====================" + "\n      '" + num1 + " " + simbEq + " "
+						+ num2 + "'"+String.format(" = %.2f", resu) + "\n====================");
+			} else {
+				JOptionPane.showMessageDialog(null,
+						"\tERRO!\nValor digitado não corresponde" + "\n as operações disponiveis.");
+			}
+			cont = Integer.parseInt(
+					JOptionPane.showInputDialog(null, "Deseja fazer outra operação" + "\n 1 - SIM | 0 - SAIR"));
 		}
-		if (eq.equals("/")) {
-			resu = num1 / num2;
-		}
-		if (eq.equals("+")) {
-			resu = num1 + num2;
-		}
-		if (eq.equals("-")) {
-			resu = num1 - num2;
-		}
-		System.out.println("A equação "+"'"+num1+" "+eq+" "+num2+"'"+" é = "+resu);
 	}
 
 }
