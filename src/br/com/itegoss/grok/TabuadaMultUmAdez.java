3f7e488821727ad93fb6e92a9f33ccbb7fcc0d25
Rafael Santana <iory20072008@gmail.com>
2016-11-29 04:17:19 -0200

    Codigo melhorado e simplificado

--- src/br/com/itegoss/grok/TabuadaMultUmAdez.java
+++ src/br/com/itegoss/grok/TabuadaMultUmAdez.java
@@ -5,12 +5,11 @@ import javax.swing.JOptionPane;
 public class TabuadaMultUmAdez {
 	public static void main(String[] args) {
 		String message = "";
-		for (int cont1 = 1; cont1 < 10; cont1++) {
-			
+		for (int cont1 = 1; cont1 <= 10; cont1++) {
 			for (int cont2 = 1; cont2 <= 10; cont2++) {
-				message += (cont1+"x"+cont2+"="+(cont1*cont2)+"\n");
+				message += (cont1+"x"+cont2+"="+(cont1*cont2)+" \n");
 			}
-			
+			message+="\n";
 		}
 		JOptionPane.showMessageDialog(null, message);
 	}
